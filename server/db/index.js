4ea1a3a6bac7e6491bf511374af3c2527565936a
Armen Rostamian <armenr@gmail.com>
2017-03-09 21:27:23 -0800

    comments

--- server/db/index.js
+++ server/db/index.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+//this is here for testing
 var mongoUri = 'mongodb://hrla13user:DictionaryAttack123!!@ds125060.mlab.com:25060/pokemon';
 
 mongoose.createConnection(mongoUri);
