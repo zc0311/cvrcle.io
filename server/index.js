8b36afb5366dab9e9faf338d561f11dec8a6100b
Armen Rostamian <armenr@gmail.com>
2017-03-13 00:24:04 -0700

    refactoring redundant code

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
-var app = require('./server.js');
-var port = 3000;
+const app = require('./server.js');
+const port = process.env.PORT || 3000;
+
 
 app.listen(port, function () {
   console.log('Cvrcle listening on port ' + port);
