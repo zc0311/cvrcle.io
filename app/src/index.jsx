711d3b2c36bc430b47276dac67f4726a51914c27
andrew yi <andrew.yi.j@gmail.com>
2017-03-14 12:58:06 -0700

    andrew's changes

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,45 +1,33 @@
 
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import SampleCard from './components/SampleCard.jsx';
-import $ from 'jquery';
 import axios from "axios";
-import { Container, Header, Card, Message, Segment, Form } from 'semantic-ui-react';
-
-import GoogleMap from './components/map.jsx';
+import ContributorEntry from './components/ContributorEntry.jsx';
 
 class AppContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      initialCenter: { 
-        lat: 33.9759, 
-        lng: -118.3907 
-      }
+      data: []
     };
   }
 
   componentDidMount() {
 
   }
-
   componentWillUnmount() {
     this.serverRequest.abort();
   }
 
   render() {
     return (
-      <Container text fluid>
-        <Header as='h1' textAlign='center'>Cvrcle.io</Header>
-        <SampleCard header='Herro Friend!' />
-        <GoogleMap initialCenter={this.state.initialCenter} />
-      </Container>
+      <div className="container">
+        <h1 className="text-center">Cvrcle</h1>
+        <ContributorEntry />
+      </div>
     );
   }
 }
 
-
-
-ReactDOM.render(
-  <AppContainer />, document.getElementById('appRoot'));
+ReactDOM.render(<AppContainer />, document.getElementById('appRoot'));
   
---
+++ server/controllers/cvrcleControllers.js
@@ -0,0 +1,25 @@
+exports.test = function(req, res) {
+  res.send(200, res);
+};
+
+exports.createOne = function (req, res) {
+
+};
+
+exports.create = function (req, res) {
+};
+
+exports.retrieveOne = function (req, res) {
+};
+
+exports.retrieveAll = function (req, res) {
+};
+
+exports.updateOne = function (req, res) {
+};
+
+exports.update = function (req, res) {
+};
+
+exports.deleteOne = function (req, res) {
+};
