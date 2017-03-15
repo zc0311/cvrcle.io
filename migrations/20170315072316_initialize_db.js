daf4a030297f7c8219873cc0c85e5cb675d58a22
Armen Rostamian <armenr@gmail.com>
2017-03-15 16:13:20 -0700

    we might not need join tables

--- migrations/20170315072316_initialize_db.js
+++ migrations/20170315072316_initialize_db.js
@@ -12,8 +12,8 @@ exports.up = function(knex, Promise) {
     })
     .createTable('itineraries', (table) => {
       table.increments('id').primary();
-      table.integer('ownerID').unsigned();
-      // table.integer('ownerID').unsigned().references('id').inTable('users').onDelete('CASCADE');
+      // table.integer('ownerID').unsigned();
+      table.integer('ownerID').unsigned().references('id').inTable('users').onDelete('CASCADE');
       table.string('itinName', 40);
       table.integer('isActive');
       table.integer('isPublic');
@@ -27,22 +27,23 @@ exports.up = function(knex, Promise) {
       table.float('lng', 10, 6);
       table.string('name', 60);
       table.string('address', 80);
-      table.string('type', 30);
-      //table.integer('contributorID').unsigned().references('id').inTable('users').onDelete('CASCADE');
-      table.integer('contributorID').unsigned();
+      table.integer('contributorID').unsigned().references('id').inTable('users').onDelete('CASCADE');
+      // table.integer('contributorID').unsigned();
+      table.integer('itinID').unsigned().references('id').inTable('itineraries').onDelete('CASCADE');
+      // table.integer('itinID').unsigned();
       table.timestamps(true);
     })
     //it creates the requisite join_tables
-    .createTable('users_itins', (table) => {
-      table.increments('id').primary();
-      table.integer('userID').unsigned().references('id').inTable('users').onDelete('CASCADE');
-      table.integer('itinID').unsigned().references('id').inTable('itineraries').onDelete('CASCADE');
-    })
-    .createTable('entries_itins', (table) => {
-      table.increments('id').primary();
-      table.integer('ID_itin').unsigned().references('id').inTable('itineraries').onDelete('CASCADE');
-      table.integer('ID_entry').unsigned().references('id').inTable('entries').onDelete('CASCADE');
-    })
+    // .createTable('users_itins', (table) => {
+    //   table.increments('id').primary();
+    //   table.integer('userID').unsigned().references('id').inTable('users').onDelete('CASCADE');
+    //   table.integer('itinID').unsigned().references('id').inTable('itineraries').onDelete('CASCADE');
+    // })
+    // .createTable('entries_itins', (table) => {
+    //   table.increments('id').primary();
+    //   table.integer('ID_itin').unsigned().references('id').inTable('itineraries').onDelete('CASCADE');
+    //   table.integer('ID_entry').unsigned().references('id').inTable('entries').onDelete('CASCADE');
+    // })
 };
 
 exports.down = function(knex, Promise) {
