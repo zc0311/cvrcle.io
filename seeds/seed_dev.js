019776fd9ec6638c2e6e7faaf666ef0c5be14a96
Armen Rostamian <armenr@gmail.com>
2017-03-15 08:59:22 -0700

    Seed file is working /

--- seeds/seed_dev.js
+++ seeds/seed_dev.js
@@ -1,13 +1,23 @@
-
-exports.seed = function(knex, Promise) {
+exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('users').insert([
+        {
+          id: 1,
+          firstName: 'Armen',
+          lastName: 'Rostamian',
+          email: 'armenr@gmail.com',
+          fbID: '10106693933724976'
+        },
+        {
+          id: 2,
+          firstName: 'Regina',
+          lastName: 'Lee',
+          email: 'regina.v.lee@gmail.com',
+          fbID: '10158329375645263'
+        }
       ]);
     });
 };
