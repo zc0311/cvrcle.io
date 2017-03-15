4f1dd48bb6bb47f53594983b6a39897b8242597e
Armen Rostamian <armenr@gmail.com>
2017-03-15 07:49:38 -0700

    build empty tables - testing

--- migrations/20170315072316_initialize_db.js
+++ migrations/20170315072316_initialize_db.js
@@ -1,8 +1,30 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema
+  // It builds the requisite core tables
+    .createTable('users', (table) => {
+      table.increments('id').primary();
+    })
+    .createTable('itineraries', (table) => {
+      table.increments('id').primary();
+    })
+    .createTable('entries', (table) => {
+      table.increments('id').primary();
+    })
+    //it creates the requisite join_tables
+    .createTable('users_itins', (table) => {
+      table.increments('id').primary();
+    })
+    .createTable('entries_itins', (table) => {
+      table.increments('id').primary();
+    })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema
+    .dropTableIfExists('users')
+    .dropTableIfExists('itineraries')
+    .dropTableIfExists('entries')
+    .dropTableIfExists('users_itins')
+    .dropTableIfExists('entries_itins')
 };
