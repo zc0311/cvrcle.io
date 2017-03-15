ca0dc9346ed96babb1e9f199ad60a99315dad94c
Armen Rostamian <armenr@gmail.com>
2017-03-15 09:30:27 -0700

    Because friends don\'t let friends work without object templates

---
+++ seeds/blank_templates.js
@@ -0,0 +1,38 @@
+// BE SURE TO FILL THINGS IN
+// BE SURE TO ADJUST 'id' FIELDS!
+
+[
+  // User template
+  {
+    id: 777,
+    firstName: 'FILL ME IN',
+    lastName: 'FILL ME IN',
+    email: 'fill@me.in',
+    fbID: '00000000000'
+  },
+
+  // Itinerary template
+  {
+    id: 777,
+    ownerID: '00000000000',
+    itinName: 'Awesome Test Itinerary',
+    isActive: '0',
+    isPublic: '1',
+  },
+
+  // Entry template
+  {
+   id: 777,
+   title: 'Go Do This Thing Bro',
+   text: 'A small descriptive paragraph goes here. Tell them what you think they should do, and why. It\'s the spectaculars.',
+   lat: 26.33333,
+   lng: 26.33333,
+   name: 'something',
+   address: '123 Yourstreet Drive, YourTown, CA, 90210',
+   type: 'restaurant'
+  },
+
+  // User mapping to Itinerary - yucky
+  // NEED TO FINISH FILLING THIS IN.
+
+];
