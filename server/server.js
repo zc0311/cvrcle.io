d0e24c5ee91add5cf987c073ed901c05289a9dc0
Armen Rostamian <armenr@gmail.com>
2017-03-16 00:47:36 -0700

    server's good to go

--- server/server.js
+++ server/server.js
@@ -10,27 +10,26 @@ const Model = require('objection').Model;
 const cors = require('cors');
 const path = require('path');
 
+// load env variables
 dotenv.load();
 
+// load database things and bind to models
 const knex = Knex(knexConfig.development);
-
 Model.knex(knex);
 
+//here be the server
 const app = express()
   .use(cors())
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
   .use(morgan('dev'))
-  .set('json spaces', 2);
+  .set('json spaces', 2)
+  .use(express.static('./app/build'));
 
+// pull server into API context
 registerApi(app);
 
-// serve up React front-end client code
-// commenting out for now while I layer in a working
-// auth0 implementation
-app.use(express.static('./app/build'));
-
-// Error handling
+// catch all errors and use next() cleverly
 app.use((err, req, res, next) => {
   if (err) {
     res.status(err.statusCode || err.status || 500).send(err.data || err.message || {});
@@ -39,6 +38,7 @@ app.use((err, req, res, next) => {
   }
 })
 
+// listen and serve
 app.listen(port, function () {
   console.log('Cvrcle listening on port ' + port);
 });
