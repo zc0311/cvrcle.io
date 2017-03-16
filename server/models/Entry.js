039169d1efc8cae7e8384a022d1d57b184d9946a
Armen Rostamian <armenr@gmail.com>
2017-03-15 23:45:30 -0700

    ES6ified these as classes, added basic validation - should revisit for specificity

--- server/models/Entry.js
+++ server/models/Entry.js
@@ -1,10 +1,28 @@
 const Model = require('objection').Model;
 
-function Entry() {
-  Model.apply(this, arguments);
+class Entry extends Model {
+  static get tableName() {
+    return 'entries'
+  }
+
+  static get jsonSchema () {
+    return {
+      type: 'object',
+      required: ['title', 'body','lat', 'lng', 'name', 'address', 'contributorID', 'itinID'],
+
+      properties: {
+        id:             { type: 'integer' },
+        title:          { type: 'string', minLength: 1, maxLength: 255 },
+        body:           { type: 'string', minLength: 1, maxLength: 255 },
+        lat:            { type: 'float', precision: 10, scale: 6 },
+        lng:            { type: 'float', precision: 10, scale: 6 },
+        name:           { type: 'string', minLength: 1, maxLength: 255 },
+        address:        { type: 'string', minLength: 1, maxLength: 255 },
+        contributorID:  { type: 'string', minLength: 1, maxLength:255 },
+        itinID:         { type: 'integer', minLength: 1 }
+      }
+    };
+  }
 }
 
-Model.extend(Entry);
 module.exports = Entry;
-
-Entry.tableName = 'entries';
--- server/models/User.js
+++ server/models/User.js
@@ -11,11 +11,11 @@ class User extends Model {
       required: ['firstName', 'lastName','email', 'fbID'],
 
       properties: {
-        id: {type: 'integer'},
-        firstName: {type: 'string', minLength: 1, maxLength: 255},
-        lastName: {type: 'string', minLength: 1, maxLength: 255},
-        email: {type: 'string', minLength:1, maxLength: 255},
-        fbID: {type: 'string', minLength: 1, maxLength: 255},
+        id:         { type: 'integer'},
+        firstName:  { type: 'string', minLength: 1, maxLength: 255 },
+        lastName:   { type: 'string', minLength: 1, maxLength: 255 },
+        email:      { type: 'string', minLength: 1, maxLength: 255 },
+        fbID:       { type: 'string', minLength: 1, maxLength: 255 }
       }
     };
   }
