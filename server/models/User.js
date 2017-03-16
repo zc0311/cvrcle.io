eaed7e59021bcf095c97076665bf1337da3ee374
Armen Rostamian <armenr@gmail.com>
2017-03-15 23:33:59 -0700

    ES6 + JSON validation schema enforcement

--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,24 @@
 const Model = require('objection').Model;
 
-function User() {
-  Model.apply(this, arguments);
+class User extends Model {
+  static get tableName() {
+    return 'users'
+  }
+
+  static get jsonSchema () {
+    return {
+      type: 'object',
+      required: ['firstName', 'lastName','email', 'fbID'],
+
+      properties: {
+        id: {type: 'integer'},
+        firstName: {type: 'string', minLength: 1, maxLength: 255},
+        lastName: {type: 'string', minLength: 1, maxLength: 255},
+        email: {type: 'string', minLength:1, maxLength: 255},
+        fbID: {type: 'string', minLength: 1, maxLength: 255},
+      }
+    };
+  }
 }
 
-Model.extend(User);
 module.exports = User;
-
-User.tableName = 'users';
