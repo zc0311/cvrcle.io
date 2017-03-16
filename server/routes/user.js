e363cc81f1201b69570bed74007367e1bfdbfcdf
Armen Rostamian <armenr@gmail.com>
2017-03-15 22:42:37 -0700

    No more sensitive data in knex.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = require('express').Router();
-const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
-const passport = require('passport');
 
 router.get('/', ensureLoggedIn, (req, res, next) => {
   res.render('user', {user: req.user});
