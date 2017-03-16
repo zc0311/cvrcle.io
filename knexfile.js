01aa89209988f081e2d8c56bf6e34b3429c4c1e6
Armen Rostamian <armenr@gmail.com>
2017-03-16 10:50:05 -0700

    force it

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: process.env.MYSQL_URI.toString(),
+      host: "cvrcle-dev.cixe27doo37n.us-west-2.rds.amazonaws.com",
       database: process.env.MYSQL_DEV_DB,
       user: process.env.MYSQL_MASTER_USER,
       password: process.env.MYSQL_MASTER_PASS
