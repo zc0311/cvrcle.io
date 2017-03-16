aed7927fe5884cfafbab3b1f3650c8c06a5ab423
Armen Rostamian <armenr@gmail.com>
2017-03-15 22:46:32 -0700

    commenting these in here to remember them

--- server/api.js
+++ server/api.js
@@ -19,3 +19,31 @@ module.exports = (app) => {
       .catch(next);
   })
 }
+
+
+
+
+
+// exports.createItin = function (req, res) {
+// };
+
+// exports.createEntry = function (req, res) {
+// };
+
+// exports.retrieveItin = function (req, res) {
+// };
+
+// exports.retrieveAllItins = function (req, res) {
+// };
+
+// exports.updateOne = function (req, res) {
+// };
+
+// exports.update = function (req, res) {
+// };
+
+// exports.deleteOne = function (req, res) {
+// };
+
+// exports.faceBookUser = function(req, res) {
+// }
