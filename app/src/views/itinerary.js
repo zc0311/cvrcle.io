4df6b3a8efdd425f30a3d2dfd797bee3f525da0f
Regina Lee <reginalee@Reginas-MBP.fios-router.home>
2017-03-17 15:28:11 -0700

    fixed the infinite loop. so close.

--- app/src/views/itinerary.js
+++ app/src/views/itinerary.js
@@ -58,7 +58,7 @@ class Itinerary extends Component {
   }
 
   render() {
-    this.getEntries()
+    // this.getEntries()
     return (
       <Provider store={store}>
         <div>
@@ -68,7 +68,6 @@ class Itinerary extends Component {
               {this.state.entries.length ? 
                 <GoogleMap 
                   locations={this.state.entries} 
-                  onChange={this.getEntries()}
                 /> : ''}
             </div>
             <div className="add-entry">
