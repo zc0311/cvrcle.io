a860b7a7e8e81e2cdff760781b438ffafa1ae651
andrew yi <andrew.yi.j@gmail.com>
2017-03-17 00:34:37 -0700

    Factored out code into different views

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,101 +1,32 @@
-// importing libraries
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
-import { Container, Header, Card, Message, Segment, Form } from 'semantic-ui-react';
+import { Card } from 'semantic-ui-react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
-import ContributorEntry from './components/ContributorEntry.jsx';
-
-import { Navbar, Nav, NavItem} from 'react-bootstrap';
-import { Router, Route, IndexRoute, Link, hashHistory } from 'react-router'
-
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-
-// importing components and files
-import GoogleMap from './containers/map.jsx';
-import AddNewEntry from './components/AddNewEntry.jsx';
-import rootReducer from './reducers/reducers_index';
-
+import Home from './views/home.js';
+import Itinerary from './views/itinerary.js';
 import Logout from './views/logout.js';
-
-let store = createStore(rootReducer)
 
 class AppContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      entries: []
-    };
-
-    this.getEntries = this.getEntries.bind(this);
-
-    this.getEntries();
-  }
-
-  // TODO: modify request to get only relevant itinerary (match itinID)
-  getEntries() {
-    axios.get('http://localhost:3000/entries')
-      .then((res) => {
-        let filteredEntries = [];
-        res.data.forEach((entry) => {
-          if (entry.itinID === 1) {
-            filteredEntries.push(entry);
-          }
-        })
-        return filteredEntries
-      })
-      .then((data) => {
-        this.setState({
-          entries: data
-        })
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-  }
-
-  componentDidMount() {
-    this.getEntries();
-  }
-
-  componentWillUnmount() {
-    this.serverRequest.abort();
   }
 
   render() {
-    // console.log('app store', store.getState());
-    return (
+    return(
       <div>
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand><a href="/">Cvrcle</a></Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <li><Link to="/logout">Logout</Link></li>
-          </Nav>
-        </Navbar>
-        <div className="container">
-          <div className="map-view">
-            {this.state.entries.length ?
-              <GoogleMap store={store} locations={this.state.entries} /> :
-              ''
-            }
-          </div>
-          <div className="add-entry">
-            <AddNewEntry data={''} />
-          </div>
-          <div className="entries">
-            <div className="ui two cards">
-              <Card.Group className="existing-entries">
-                {this.state.entries.length ?
-                  (this.state.entries.map((entryData, i) => (
-                    <ContributorEntry key={i} {...entryData} />))) :
-                  <div className="text-center">"No entries yet!"</div>
-                }
-              </Card.Group>
-            </div>
-          </div>
+        <h1 className="cvrcle-logo text-center">
+          <span style={{color: '#4885ed'}}>C</span>
+          <span style={{color: '#db3236'}}>V</span>
+          <span style={{color: '#f4c20d'}}>R</span>
+          <span style={{color: '#4885ed'}}>C</span>
+          <span style={{color: '#3cba54'}}>L</span>
+          <span style={{color: '#db3236'}}>E</span>
+        </h1>
+
+        <div>
+          {/* replace below div with auth0 */}
         </div>
       </div>
     );
@@ -105,7 +36,8 @@ class AppContainer extends Component {
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={AppContainer} />
-    {/* routes go here */}
+    <Route path="/home" component={Home} />
+    <Route path="/itinerary" component={Itinerary} />
     <Route path="/logout" component={Logout} />
   </Router>
 ), document.getElementById('appRoot'))
