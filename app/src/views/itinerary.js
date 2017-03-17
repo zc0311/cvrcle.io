7ec7ca575f2ce15736c58f2b421a2317af06d963
Regina Lee <reginalee@Reginas-MBP.fios-router.home>
2017-03-17 11:29:43 -0700

    fixed redux provider bug

--- app/src/views/itinerary.js
+++ app/src/views/itinerary.js
@@ -1,18 +1,14 @@
-// importing libraries
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from "axios";
 import { Container, Header, Card, Message, Segment, Form } from 'semantic-ui-react';
-
 import ContributorEntry from '../components/ContributorEntry.jsx';
-
-// importing components and files
 import GoogleMap from '../containers/map.jsx';
 import AddNewEntry from '../components/AddNewEntry.jsx';
 import rootReducer from '../reducers/reducers_index';
-
+import { Provider } from 'react-redux';
+import store from '../store.js';
 import Navbar from '../components/navbar.jsx';
-
 
 class Itinerary extends Component {
   constructor(props) {
@@ -55,29 +51,31 @@ class Itinerary extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar />
-        <div className="container">
-          <div className="map-view">
-            {this.state.entries.length ? 
-              <GoogleMap locations={this.state.entries} /> : ''}
-          </div>
-          <div className="add-entry">
-            <AddNewEntry data={''} />
-          </div>
-          <div className="entries">
-            <div className="ui two cards">
-              <Card.Group className="existing-entries">
-                {this.state.entries.length ?
-                  (this.state.entries.map((entryData, i) => (
-                    <ContributorEntry key={i} {...entryData} />))) :
-                  <div className="text-center">"No entries yet!"</div>
-                }
-              </Card.Group>
+      <Provider store={store}>
+        <div>
+          <Navbar />
+          <div className="container">
+            <div className="map-view">
+              {this.state.entries.length ? 
+                <GoogleMap locations={this.state.entries} /> : ''}
+            </div>
+            <div className="add-entry">
+              <AddNewEntry data={''} />
+            </div>
+            <div className="entries">
+              <div className="ui two cards">
+                <Card.Group className="existing-entries">
+                  {this.state.entries.length ?
+                    (this.state.entries.map((entryData, i) => (
+                      <ContributorEntry key={i} {...entryData} />))) :
+                    <div className="text-center">"No entries yet!"</div>
+                  }
+                </Card.Group>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
