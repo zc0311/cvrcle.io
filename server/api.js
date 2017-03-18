bf1011b2bd67f15b8e74a823bce2a63dc0ae3a1f
Armen Rostamian <armenr@gmail.com>
2017-03-18 12:34:04 -0700

    cleans up the things

--- server/api.js
+++ server/api.js
@@ -90,27 +90,3 @@ module.exports = (app) => {
   })
 
 };
-
-// exports.createItin = function (req, res) {
-// };
-
-// exports.createEntry = function (req, res) {
-// };
-
-// exports.retrieveItin = function (req, res) {
-// };
-
-// exports.retrieveAllItins = function (req, res) {
-// };
-
-// exports.updateOne = function (req, res) {
-// };
-
-// exports.update = function (req, res) {
-// };
-
-// exports.deleteOne = function (req, res) {
-// };
-
-// exports.faceBookUser = function(req, res) {
-// }
